46c13cde17d648d4d029d4768b78ce2faae1e0c8
Umang Boriwala <umang0243@yahoo.co.in>
2017-09-10 01:29:29 +0530

    Created project structure for online shopping

---
+++ myonlineshoppingbackend/src/main/java/com/umang/myonlineshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.umang.myonlineshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
